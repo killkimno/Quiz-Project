a2123556fdeb321fa7bee511004d4ac88aa70bd0
Monkeyhead <killkimno@hotmail.com>
2018-02-22 23:47:00 +0900

    no message

---
+++ Assets/Editor/QuestionObjectEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(QuestionObject))]
+public class QuestionObjectEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        QuestionObject aa = target as QuestionObject;
+        if (GUILayout.Button("Build Object"))
+        {
+          //  myScript.BuildObject();
+        }
+    }
+
+
+}
