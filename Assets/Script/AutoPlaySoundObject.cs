ff64fe23199c30095b1e8c375246109e5c3872a6
Monkeyhead <killkimno@hotmail.com>
2018-02-24 03:33:24 +0900

    a
    a

---
+++ Assets/Script/AutoPlaySoundObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoPlaySoundObject : MonoBehaviour {
+
+    public SoundMgr.SoundType soundType;
+
+
+
+    private void OnEnable()
+    {
+        SoundMgr.PlaySound(soundType);
+    }
+}
--- Assets/Script/SoundMgr.cs
+++ Assets/Script/SoundMgr.cs
@@ -6,13 +6,18 @@ public class SoundMgr : MonoBehaviour {
 
     public enum SoundType
     {
-        None, SlotOpen, SlotEnter,
+        None, SlotOpen, SlotEnter, Fortune, question_name, sound_buttion_right, sound_buttion_wrong, answer_confirm,
     };
 
     public static SoundMgr instance;
 
     public AudioSource clickSound;
     public AudioSource openSound;
+    public AudioSource fortuneSound;
+    public AudioSource question_name;
+    public AudioSource sound_buttion_right;
+    public AudioSource sound_buttion_wrong;
+    public AudioSource answer_confirm;
 
     private void Awake()
     {
@@ -28,6 +33,26 @@ public class SoundMgr : MonoBehaviour {
         else if(soundType == SoundType.SlotEnter)
         {
             instance.openSound.Play();
+        }
+        else if(soundType == SoundType.Fortune)
+        {
+            instance.fortuneSound.Play();
+        }
+        else if(soundType == SoundType.question_name)
+        {
+            instance.question_name.Play();
+        }
+        else if(soundType == SoundType.sound_buttion_right)
+        {
+            instance.sound_buttion_right.Play();
+        }
+        else if(soundType == SoundType.sound_buttion_wrong)
+        {
+            instance.sound_buttion_wrong.Play();
+        }
+        else if (soundType == SoundType.answer_confirm)
+        {
+            instance.answer_confirm.Play();
         }
     }
 
